1ec30a7195118d152df95327f83ef040d0a6bca2
Callum Worrall <callum.worrall71@gmail.com>
2020-12-07 09:22:13 +1100

    Routing Setup

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import SignIn_Page from './pages/SignIn_Page';
+
+export default function App()
+{
+  return (
+    <div className="App">
+      <SignIn_Page/>
+    </div>
+  )
+}
---
+++ src/components/pages/SignIn_Page.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function SignIn_Page(props)
+{
+  return( <div class="page">
+
+    <h1>Sign In</h1>
+
+  </div> )
+}
