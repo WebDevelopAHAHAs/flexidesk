bad473d2b9deafd438bec304eebde7801b06d57e
Callum Worrall <callum.worrall71@gmail.com>
2020-12-07 11:14:15 +1100

    Folder restructure

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
 import SignInPage from './universal/SignInPage';
 
-import EmployeesPage from './admin/employee/EmployeesPage';
+import EmployeesPage from './admin/employees/EmployeesPage';
 
 export default function App()
 {
   return (
     <div className="App">
       <BrowserRouter>
+        <Route to="/" render={(props) => <SignInPage/> } />
         <Route to="/employees" render={(props) => <EmployeesPage/> } />
       </BrowserRouter>
     </div>
