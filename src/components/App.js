beecff196b41148054bf6fef65076983dca29355
Thomas Fisher <thomasjfisher1@gmail.com>
2020-12-07 12:20:06 +1100

    Added NavBar component

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,19 @@ import {BrowserRouter, Route} from 'react-router-dom'
 import SignInPage from './sign_in/SignInPage';
 import ViewEmployeesPage from './users/admin/ViewEmployeesPage';
 import DashboardPageAdmin from './dashboard/admin/DashboardPageAdmin';
+import NavBar from './NavBar'
 
 export default function App()
 {
   return (
     <div className="App">
       <BrowserRouter>
-        <Route to="/" render={(props) => <SignInPage/> } />
-        <Route to="/admin/dashboard" render={(props) => <DashboardPageAdmin/> } />
-        <Route to="/admin/employees" render={(props) => <ViewEmployeesPage/> } />        
+      <Route to="/admin/dashboard" render={(props) => <DashboardPageAdmin/> } />
+      <NavBar/>
+
+        {/* <Route to="/" render={(props) => <SignInPage/> } /> */}
+
+        {/* <Route to="/admin/employees" render={(props) => <ViewEmployeesPage/> } />         */}
       </BrowserRouter>
     </div>
   )
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div>
+            <h1>NavBar</h1>
+            <ul>
+                <li><a href="/flexidesk">Home</a></li>
+                <li><a href="/viewbookings">View Bookings</a></li>
+                <li><a href="/newbooking">New Bookings</a></li>
+                <li><a href="/viewemployees">View Employees</a></li>
+                <li><a href="/viewdesks">View Desks</a></li>
+                <li><a href="/analytics">Analytics</a></li>
+            </ul>
+        </div>
+    )
+}
