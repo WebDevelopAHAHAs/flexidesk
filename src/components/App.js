1dc43edcc6d73b5a7d52bd9d1172553e1fbc3342
Callum Worrall <callum.worrall71@gmail.com>
2020-12-07 12:11:51 +1100

    Exact Route Paths Updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
 import React from 'react'
 import {BrowserRouter, Route} from 'react-router-dom'
+
 import SignInPage from './sign_in/SignInPage';
 import ViewEmployeesPage from './users/admin/ViewEmployeesPage';
 import DashboardPageAdmin from './dashboard/admin/DashboardPageAdmin';
 
 export default function App()
 {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Route to="/" render={(props) => <SignInPage/> } />
-        <Route to="/admin/dashboard" render={(props) => <DashboardPageAdmin/> } />
-        <Route to="/admin/employees" render={(props) => <ViewEmployeesPage/> } />        
-      </BrowserRouter>
-    </div>
-  )
+  return ( <div className="App">
+    <BrowserRouter>
+      <Route exact path="/" render={(props) => <SignInPage/> } />
+      <Route exact path="/admin/dashboard" render={(props) => <DashboardPageAdmin/> } />
+      <Route exact path="/admin/viewemployees" render={(props) => <ViewEmployeesPage/> } />
+    </BrowserRouter>
+  </div> )
 }
