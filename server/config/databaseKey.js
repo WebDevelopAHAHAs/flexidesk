f465bc0693544f294e54ca0625aadc330ca7cd13
Callum-Worrall <callum.worrall71@gmail.com>
2021-01-07 16:48:55 +1100

    Delete databaseKey.js

--- server/config/databaseKey.js
+++
@@ -1,7 +0,0 @@
-const uri = "mongodb+srv://callumworrall:crtpassword@flexidesk.oisai.mongodb.net/flexidesk?retryWrites=true&w=majority";
-
-module.exports = {
-  uri
-}
-
-//passwordAuth: crtpassword
