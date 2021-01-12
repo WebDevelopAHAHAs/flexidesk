d08ebe26bfc3f762d8f58b28eed5d11e62ed8fe0
Callum Worrall <callum.worrall71@gmail.com>
2021-01-12 17:01:12 +1100

    Edit Form Placeholder information appearing, tidied up the edit form.
    Also brazeningly considering making our forms even simpler to use, by making a universal modalForm. We shall see.

--- client/src/components/admin/employees/EditEmployeeModal.js
+++ client/src/components/admin/employees/EditEmployeeModal.js
@@ -1,36 +1,42 @@
 // import React from 'react'
+import {useState} from 'react'
 import {Modal, Backdrop, Fade} from '@material-ui/core/';
 
 import EditEmployeeForm from './EmployeeEditForm'
 
 export default function EditEmployee(props) {
 
-  const handleOpen = (event) => {
-    console.log("Edit Employee Button Pressed.")
-    // console.log(event.target.id)
-    props.setOpen(true);
-    props.setEditNum(event.target.id) //event.target.id)
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = (event) => {    
+    console.log("Edit Employee Button Pressed.", props)
+    setOpen(true);
   };
 
   const handleClose = (success) => {
-      props.setOpen(false);
-      if(success) window.location.reload()  
+      setOpen(false);
+      if(success === true) window.location.reload()  
   };
 
   return(<div className='editEmployeeDiv'>
     
-    <button id="Edit" onClick={handleOpen}> <i className="far fa-edit"></i></button>
+    <button id="Edit" onClick={handleOpen}> <i className="far fa-edit"></i></button>    
 
     <Modal className="modal-position"
-      open={props.open} onClose={handleClose} closeAfterTransition
+      open={open} onClose={handleClose} closeAfterTransition
       aria-labelledby="transition-modal-title" aria-describedby="transition-modal-description"
       BackdropComponent={Backdrop} BackdropProps={{ timeout: 500 }} >
 
-      <Fade className="modal-styling" in={props.open}>
-         <div>
+      <Fade className="modal-styling" in={open}>
+          <div>
 
-            <h2 id="transition-modal-title">{props.editNum} Edit Employee</h2>
-            <p id="transition-modal-description"> <EditEmployeeForm handleClose={handleClose}/> </p>
+            <h2 id="transition-modal-title">Edit Employee</h2>
+            <div id="transition-modal-description">
+              <EditEmployeeForm handleClose={handleClose}
+                first_name={props.first_name}
+                email={props.email}
+              />
+            </div>
           
           </div>
       </Fade>
