a1493b4eda1b563543a0e0cac2c7ec38f139386d
Thomas Fisher <thomasjfisher1@gmail.com>
2021-01-12 16:15:09 +1100

    styled modal

--- client/src/components/styling/useStyles.js
+++ client/src/components/styling/useStyles.js
@@ -67,7 +67,7 @@ const useStyles = makeStyles((theme) => ({
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
-    width: theme.spacing(7),
+    width: theme.spacing(0),
     [theme.breakpoints.up('sm')]: {
       width: theme.spacing(0),
     },
