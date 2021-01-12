24f68b122604af5063f8954dab5f72c740566419
Callum Worrall <callum.worrall71@gmail.com>
2021-01-13 10:11:17 +1100

    Update databaseKey.js

--- server/config/databaseKey.js
+++ server/config/databaseKey.js
@@ -1,7 +1,5 @@
-const uri = "mongodb+srv://thomas-fisher:thunder00@flexidesk.oisai.mongodb.net/flexidesk?retryWrites=true&w=majority";
+const uri = "mongodb+srv://developer:crtpassword@flexidesk.oisai.mongodb.net/flexidesk?retryWrites=true&w=majority";
 
-module.exports = {
-uri
-}
+module.exports = { uri }
 
 //passwordAuth: crtpassword
