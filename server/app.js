4c9b5e24b9f9ecbdf4311884f102b37dd663bdc1
Rachel Williams <rachel.williams@leapdev.io>
2021-01-19 11:39:36 +1100

    updated url to htpp

--- server/app.js
+++ server/app.js
@@ -45,7 +45,7 @@ app.use(session({
 const corsOptions = {
   credentials: true
 }
-const whitelist = ['http://localhost:3000', 'www.flexi-desks.com/', 'https://flexidesk.herokuapp.com/']
+const whitelist = ['http://localhost:3000', 'http://flexi-desks.com/', 'https://flexidesk.herokuapp.com/']
 app.use(cors({
   credentials: true,
   origin: function (origin,callback) {
