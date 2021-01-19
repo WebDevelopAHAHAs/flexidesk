9873758aa441e8c4fead8bde64ced719fdd307ac
Rachel Williams <rachel.williams@leapdev.io>
2021-01-19 21:03:00 +1100

    changed AP to flexidesks

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Create an axios instance
 export default axios.create({
-  baseURL: 'http://localhost:3009',
+  baseURL: 'https://flexidesk.herokuapp.com',
   timeout: 5000,
   withCredentials: true
 })
